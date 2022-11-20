e1a4df0603617c2991c32d740043521f512fcb8f
sdkmax <118674708+sdkmax@users.noreply.github.com>
2022-11-20 21:29:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -78,7 +78,7 @@ global.mess = {
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
+    wait: '👽🙂Pleas Wait...',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     linkm: 'Where is the link?',
